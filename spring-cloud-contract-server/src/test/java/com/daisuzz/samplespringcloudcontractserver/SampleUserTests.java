0055731b312cfe270514f2cb747d70ec03dd7eae
daisuzz <daisaku.rugby@gmail.com>
2020-10-16 23:43:25 +0900

    delete unnecessary annotation

--- spring-cloud-contract-server/src/test/java/com/daisuzz/samplespringcloudcontractserver/SampleUserTests.java
+++ spring-cloud-contract-server/src/test/java/com/daisuzz/samplespringcloudcontractserver/SampleUserTests.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.context.WebApplicationContext;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SampleUserTests {
 
